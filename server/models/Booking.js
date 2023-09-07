92f479249e1cc233f2b771bd409a08e0d12f5581
Umar Ahmed <umarahmedse@outlook.com>
2023-09-07 09:08:36 -0700

    added pre middleware to check uniqueness(booking)

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
+import AppError from "../utils/appError.js";
 const BookingSchema = new mongoose.Schema(
   {
     userId: {
@@ -20,6 +21,19 @@ const BookingSchema = new mongoose.Schema(
 
   { timestamps: true }
 );
+BookingSchema.pre("save", async function (next) {
+  const existingBooking = await this.constructor.findOne({
+    userId: this.userId,
+    courseId: this.courseId,
+  });
 
+  if (existingBooking && !existingBooking._id.equals(this._id)) {
+    // If a duplicate booking is found for the same user and course, prevent saving
+    const error = new AppError("User is already booked into this course.", 404);
+    return next(error);
+  }
+
+  next();
+});
 const Booking = mongoose.model("Booking", BookingSchema);
 export default Booking;
