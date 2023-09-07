c147cd960851c7ff83ae032b7473af45ea050e81
Umar Ahmed <umarahmedse@outlook.com>
2023-09-07 09:06:14 -0700

    bug fixes

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,14 +1,15 @@
 import Booking from "../models/Booking.js";
+import catchAsync from "../utils/catchAsync.js";
 export const createBooking = catchAsync(async (req, res, next) => {
   const bookingBody = {
     userId: req.body.userId,
-    courseId: req.body.courseId,
+    courseId: req.params.courseId,
     amount: req.body.amount,
   };
   const booking = await Booking.create(bookingBody);
 
   res.status(200).json({
     status: "success",
-    course,
+    booking,
   });
 });
