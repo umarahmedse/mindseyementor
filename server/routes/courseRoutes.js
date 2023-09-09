9fddd78d78e8593a44dbce1f46cd4de34f22970c
Umar Ahmed <umarahmedse@outlook.com>
2023-09-09 08:20:22 -0700

    added route to get complete course detail (fixed)

--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -10,7 +10,7 @@ import { protect, restrictTo } from "../controllers/authController.js";
 const router = express.Router();
 router
   .get("/details/:id", protect, getCourse)
-  .get("/courseDetail/:courseId", protect, getCompleteCourse)
+  .get("/coursedetail/:courseId", protect, getCompleteCourse)
   .delete("/:id", protect, restrictTo("admin"), deleteCourse)
   .post("/add", protect, restrictTo("admin", "psychologist"), createCourse)
   .patch(
