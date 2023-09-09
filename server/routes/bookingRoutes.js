aa61cf0c37e7bcdcaeb9f24320ac06681329fb87
Umar Ahmed <umarahmedse@outlook.com>
2023-09-09 04:38:04 -0700

    deleted booking named files

--- server/routes/bookingRoutes.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import { createBooking } from "../controllers/bookingController.js";
-const router = express.Router();
-router.post("/new/:courseId", createBooking);
-export default router;
