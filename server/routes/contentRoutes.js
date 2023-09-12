07cd26b2fe3023ec99219b6ae6308134c0ac8ea6
Umar Ahmed <umarahmedse@outlook.com>
2023-09-11 21:45:01 -0700

    added patch route to edit content

--- server/routes/contentRoutes.js
+++ server/routes/contentRoutes.js
@@ -1,12 +1,22 @@
 import express from "express";
-import { addContentToCourse } from "../controllers/contentController";
+import {
+  addContentToCourse,
+  editCourseContent,
+} from "../controllers/contentController";
 import { protect, restrictTo } from "../controllers/authController.js";
 const router = express.Router();
 
-router.post(
-  "/add/:id",
-  protect,
-  restrictTo("psychologist", "admin"),
-  addContentToCourse
-);
+router
+  .post(
+    "/add/:id",
+    protect,
+    restrictTo("psychologist", "admin"),
+    addContentToCourse
+  )
+  .patch(
+    "/update/:id",
+    protect,
+    restrictTo("psychologist", "admin"),
+    editCourseContent
+  );
 export default router;
