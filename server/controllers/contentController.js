58efd0887901fe948a1ac83b344611e927aa4237
Umar Ahmed <umarahmedse@outlook.com>
2023-09-11 21:44:47 -0700

    added edit content handler

--- server/controllers/contentController.js
+++ server/controllers/contentController.js
@@ -25,3 +25,27 @@ export const addContentToCourse = catchAsync(async (req, res, next) => {
     content,
   });
 });
+
+export const editCourseContent = catchAsync(async (req, res, next) => {
+  const courseId = req.params.id;
+  const contentBody = req.body;
+  contentBody.courseId = courseId;
+  const course = await Course.findById(courseId);
+  if (
+    course.offeredBy.toString() !== req.user.id ||
+    req.user.role !== "admin"
+  ) {
+    return next(
+      new AppError(
+        "You Don't Have Permission To Add Content To This Course",
+        401
+      )
+    );
+  }
+  const content = await Content.findByIdAndUpdate(courseId, contentBody);
+
+  res.status(200).json({
+    status: "success",
+    content,
+  });
+});
